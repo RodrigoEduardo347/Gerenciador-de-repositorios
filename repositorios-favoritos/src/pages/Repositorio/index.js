cb30f5c12d201d9da28efd6d8575e0cb44a6862e
RodrigoOpyt <rodrigo.growth.opyt@gmail.com>
2022-12-09 17:57:12 -0300

    Projeto repositorios-favoritos finalizado

--- repositorios-favoritos/src/pages/Repositorio/index.js
+++ repositorios-favoritos/src/pages/Repositorio/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { BackButton, Container, Owner, IssuesList } from "./style";
+import { BackButton, Container, Owner, IssuesList, PageActions, FilterList } from "./style";
 import { api } from '../../services/api'
 import Loading from "../../components/loading/Loading";
 import { FaArrowLeft } from "react-icons/fa";
@@ -10,8 +10,15 @@ export default function Repositorio() {
     const [repositorioAtual, setRepositorioAtual] = useState('');
     const [issues, setIssues] = useState([]);
     const [loading, setLoading] = useState(true);
-
+    const [page, setPage] = useState(1);
     const { repositorio } = useParams();
+    const [filters, setFilters] = useState([
+        { state: 'all', label: 'Todas', active: true },
+        { state: 'open', label: 'Abertas', active: false },
+        { state: 'closed', label: 'Fechadas', active: false },
+    ]);
+    const [filterIndex, setFilterIndex] = useState(0);
+
     useEffect(() => {
         async function load() {
             const [repositorioData, issuesData] = await Promise.all([
@@ -32,6 +39,30 @@ export default function Repositorio() {
         load();
     }, [repositorio]);
 
+    function handlePage(action) {
+        setPage(action === 'back' ? page - 1 : page + 1)
+    }
+
+    function handleFilter(index) {
+        setFilterIndex(index);
+    }
+
+    useEffect(() => {
+        async function loadIssue() {
+            const response = await api.get(`repos/${repositorio}/issues`, {
+                params: {
+                    state: filters[filterIndex].state,
+                    page,
+                    per_page: 5,
+                }
+            });
+            setIssues(response.data);
+        };
+
+        loadIssue();
+
+    }, [filters, filterIndex, page]);
+
     if (loading) {
         return (
             <>
@@ -39,7 +70,6 @@ export default function Repositorio() {
             </>
         )
     } else {
-        console.log(issues)
         return (
             <>
                 <Container>
@@ -56,10 +86,23 @@ export default function Repositorio() {
                         <p>{repositorioAtual.description}</p>
                     </Owner>
 
+                    <FilterList active={filterIndex}>
+                        {filters.map((filter, index) => (
+                            <button
+                                type="button"
+                                key={filter.label}
+                                onClick={() => handleFilter(index)}
+                            >
+                                {filter.label}
+                            </button>
+                        ))}
+                    </FilterList>
+
                     <IssuesList>
                         {issues.map(issue => (
                             <li key={String(issue.id)}>
                                 <img src={issue.user.avatar_url} alt={issue.user.login} />
+
                                 <div>
                                     <strong>
                                         <a href={issue.html_url}>{issue.title}</a>
@@ -67,14 +110,33 @@ export default function Repositorio() {
                                         {issue.labels.map(label => (
                                             <span key={String(label.id)}>{label.name}</span>
                                         ))}
+
                                     </strong>
 
                                     <p>{issue.user.login}</p>
+
                                 </div>
 
                             </li>
                         ))}
                     </IssuesList>
+
+                    <PageActions>
+                        <button
+                            type="button"
+                            onClick={() => handlePage("back")}
+                            disabled={page < 2}
+                        >
+                            Voltar
+                        </button>
+
+                        <button
+                            type="button"
+                            onClick={() => handlePage("next")}
+                        >
+                            Próxima
+                        </button>
+                    </PageActions>
                 </Container>
             </>
         )
